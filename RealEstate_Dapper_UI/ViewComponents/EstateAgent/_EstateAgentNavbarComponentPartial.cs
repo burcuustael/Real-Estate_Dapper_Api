7e2b9a6a084cc018507ecb046e6fee9d9be34cd1
burcuustael <burcustael@gmail.com>
2024-05-11 19:42:21 +0300

    denemeee...

---
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentNavbarComponentPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent;
+
+public class _EstateAgentNavbarComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
